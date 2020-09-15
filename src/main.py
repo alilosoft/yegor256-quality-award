64cf4fc3c7b33e58e988a3b232eb59e55da068ce
alilosoft <fellahi.ali@gmail.com>
2020-09-15 04:50:27 +0100

    #1 skeleton

--- src/main.py
+++ src/main.py
@@ -1,4 +1,12 @@
 import requests
 
-r = requests.get('https://www.yegor256.com/2019/11/03/award-2020.html')
-print(r.text)
+# get all projects from https://www.yegor256.com/2019/11/03/award-2020.html
+html = requests.get('https://www.yegor256.com/2019/11/03/award-2020.html')
+print(html.text)
+
+# get HoC: https://hitsofcode.com/github/<user>/<repo>/json
+
+# get LoC: https://api.codetabs.com/v1/loc/?github==<username>/<reponame>
+
+# check age > 1y: https://api.github.com/repos/<user>/<repo>/commits?until=2019-09-01
+
